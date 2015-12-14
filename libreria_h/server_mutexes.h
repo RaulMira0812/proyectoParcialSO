bbd4be950b8ac59f85843f86d192bd91f8f4a0c8
henryxavier10 <hlasso@espol.edu.ec>
2015-12-14 19:18:58 +0000

    correciones

--- libreria_h/server_mutexes.h
+++ libreria_h/server_mutexes.h
@@ -2,10 +2,9 @@
 #define SERVER_MUTEX
 #include <pthread.h>
 
-pthread_mutex_t socket_mutex; //Guarantee 2 threads don't simultaneously write to client socket
-pthread_mutex_t usuarios_mutex; //Guarantee 2 threads don't both modify user list
-pthread_mutex_t canal_mutex; //Guarantee 2 threads don't both modify chat room list
-//Must be called by server code before command loop
+pthread_mutex_t socket_mutex; //Valida que dos hilos no sobreescriban simultaneamente el socket del cliente
+pthread_mutex_t usuarios_mutex; //Valida que dos hilos de usuarios no modifiquen simultaneamente la lista de usuarios
+pthread_mutex_t canal_mutex; //Valida que dos hilos no mocdifiquen simultaneamente la lista de canales
 
 void inicializar_mutexes();
 #endif
